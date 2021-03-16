dd98f41c76e26aa93f2191ba82f2e5dbb5f37d61
Tdartigues <73830061+Tdartigues@users.noreply.github.com>
2021-03-16 16:18:02 +0100

    Init Master

---
+++ GoStyle/GoStyle/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using GoStyle.Services;
+using GoStyle.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GoStyle
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
