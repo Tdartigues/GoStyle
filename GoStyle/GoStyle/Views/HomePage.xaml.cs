3ac82ac798eb747a8f924c3189422df70d5ec8e1
Flavyen <flavyen.tonnelier@epsi.fr>
2021-03-16 16:51:52 +0100

    AddHomePage
    comme le porc salut, c'est écrit dessus.

---
+++ GoStyle/GoStyle/Views/HomePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GoStyle.Views
+{
+    public partial class HomePage : ContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        var logoApp = new Image {
+            Source = ImageSource.FromUri(
+                new Uri("https://media.discordapp.net/attachments/799566225088577588/821362139226439680/GOSTYLE.PNG")
+            ) };
+        }
+    }
+}
