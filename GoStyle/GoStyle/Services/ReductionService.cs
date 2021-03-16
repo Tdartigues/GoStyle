035599984abd531107cf85d0b30f5038ad54a407
Tdartigues <73830061+Tdartigues@users.noreply.github.com>
2021-03-16 17:01:45 +0100

    Reduction Services to singleton

--- GoStyle/GoStyle/Services/ReductionService.cs
+++ GoStyle/GoStyle/Services/ReductionService.cs
@@ -1,15 +1,34 @@
 ﻿using GoStyle.Models;
 using Tiny.RestClient;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace GoStyle.Services
 {
     public class ReductionServices
     {
-        public Reduction GetReductionAsync(string uId)
+        ReductionServices _reductionServices;
+        TinyRestClient _client;
+
+        private ReductionServices()
         {
-            TinyRestClient client = new TinyRestClient(new HttpClient(), "http://127.0.0.1:8000/QRCode/");
-            return client.GetRequest("Reduction/" + uId).ExecuteAsync<Reduction>().Result;
+            _client = new TinyRestClient(new HttpClient(), "http://127.0.0.1:8000/QRCode/");
+        }
+
+        public ReductionServices GetReductionServices()
+        {
+            if(_reductionServices is null)
+            {
+                _reductionServices = new ReductionServices();
+                return _reductionServices;
+            }else
+            {
+                return _reductionServices;
+            }
+        }
+        public async Task<Reduction> GetReductionAsync(string uId)
+        {
+            return await _client.GetRequest("Reduction/" + uId).ExecuteAsync<Reduction>();
         }
     }
 }
