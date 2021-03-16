13b286b1b8cb9f2ff160e8bbd1bb64265a53c907
Flavyen <flavyen.tonnelier@epsi.fr>
2021-03-16 17:01:57 +0100

    Add Correctif path
    change aboutpage to homepage

--- GoStyle/GoStyle/ViewModels/LoginViewModel.cs
+++ GoStyle/GoStyle/ViewModels/LoginViewModel.cs
@@ -1,4 +1,4 @@
-﻿using GoStyle.Views;
+using GoStyle.Views;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -18,7 +18,7 @@ namespace GoStyle.ViewModels
         private async void OnLoginClicked(object obj)
         {
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
         }
     }
 }
