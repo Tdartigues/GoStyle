b2c8b03a7261a0d660bb8afaf8aca2a069c1272c
Tdartigues <73830061+Tdartigues@users.noreply.github.com>
2021-03-16 16:24:28 +0100

    Init BackEnd

---
+++ GoStyle/GoStyle/Models/Reduction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoStyle.Models
+{
+    public class Reduction
+    {
+        public string id { get; set; }
+        public string nom { get; set; }
+        public int taux { get; set; }
+    }
+}
---
+++ GoStyle/GoStyle/Services/ReductionService.cs
@@ -0,0 +1,15 @@
+﻿using GoStyle.Models;
+using Tiny.RestClient;
+using System.Net.Http;
+
+namespace GoStyle.Services
+{
+    public class ReductionServices
+    {
+        public Reduction GetReductionAsync(string uId)
+        {
+            TinyRestClient client = new TinyRestClient(new HttpClient(), "http://127.0.0.1:8000/QRCode/");
+            return client.GetRequest("Reduction/" + uId).ExecuteAsync<Reduction>().Result;
+        }
+    }
+}
